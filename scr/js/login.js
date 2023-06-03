31fea74207ae63ea4448db930801fe082bda3681
Maxtasi <magomatioficial@gmail.com>
2023-06-02 21:14:40 -0300

    menu_terminado

---
+++ scr/js/login.js
@@ -0,0 +1,18 @@
+document.getElementById('forgotPasswordLink').addEventListener('click', function(e) {
+    e.preventDefault();
+    // Lógica para recuperar la contraseña
+    alert('Funcionalidad de recuperación de contraseña aún no implementada.');
+  });
+  
+  document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Lógica para iniciar sesión
+    alert('Funcionalidad de inicio de sesión aún no implementada.');
+  });
+  
+  document.getElementById('registrationForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Lógica para el registro
+    alert('Funcionalidad de registro aún no implementada.');
+  });
+  
