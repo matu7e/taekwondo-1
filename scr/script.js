07895040f7850dc433d1f0e9743b367a2e22905b
Maxtasi <magomatioficial@gmail.com>
2023-06-04 16:10:48 -0300

    actualizacion

--- scr/script.js
+++ scr/script.js
@@ -13,6 +13,24 @@ checkBtn.addEventListener('click', function() {
   contenedorMenu.style.left = menuVisible ? '-100%' : '0';
 });
 
+
+// controla el funcionamiento del boton de wpp
 document.getElementById("whatsapp-btn").onclick = function() {
   window.location.href = "https://wa.me/3512343686?text=Hola,%20quiero%20info%20sobre%20las%20clases%20de%20taekwondo";
 };
+
+
+
+// bloquear scrol vertical al hacer clik en el menu de hambiurguesa
+var scrollEnabled = true;
+var label = document.querySelector(".checkbtn");
+
+label.addEventListener("click", function() {
+  scrollEnabled = !scrollEnabled;
+  
+  if (scrollEnabled) {
+    document.body.style.overflow = "auto";  // Habilitar el scroll
+  } else {
+    document.body.style.overflow = "hidden";  // Deshabilitar el scroll
+  }
+});
