4c4dbf38fde12ef8bff509c68e360a2dbf35ca8d
Maxtasi <magomatioficial@gmail.com>
2023-06-12 19:40:45 -0300

    pagina taekwondo

---
+++ js/cintos.js
@@ -0,0 +1,43 @@
+const descripcionBlanco = document.getElementById("contenedor-descripcion-blanco");
+const botonCintoBlanco = document.querySelector(".button-blanco");
+
+botonCintoBlanco.addEventListener("click", () => {
+  descripcionBlanco.classList.toggle("mostrar");
+});
+
+
+const descripcionAmarillo = document.getElementById("contenedor-descripcion-amarillo");
+const botonCintoAmarillo = document.querySelector(".button-amarillo");
+
+botonCintoAmarillo.addEventListener("click", () => {
+  descripcionAmarillo.classList.toggle("mostrar");
+});
+
+const descripcionVerde = document.getElementById("contenedor-descripcion-verde");
+const botonCintoVerde = document.querySelector(".button-verde");
+
+botonCintoVerde.addEventListener("click", () => {
+  descripcionVerde.classList.toggle("mostrar");
+});
+
+const descripcionAzul = document.getElementById("contenedor-descripcion-azul");
+const botonCintoAzul = document.querySelector(".button-azul");
+
+botonCintoAzul.addEventListener("click", () => {
+  descripcionAzul.classList.toggle("mostrar");
+});
+
+const descripcionRojo = document.getElementById("contenedor-descripcion-rojo");
+const botonCintoRojo = document.querySelector(".button-rojo");
+
+botonCintoRojo.addEventListener("click", () => {
+  descripcionRojo.classList.toggle("mostrar");
+});
+
+
+const descripcionNegro = document.getElementById("contenedor-descripcion-negro");
+const botonCintoNegro = document.querySelector(".button-negro");
+
+botonCintoNegro.addEventListener("click", () => {
+  descripcionNegro.classList.toggle("mostrar");
+});
