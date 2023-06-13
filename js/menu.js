dfc9389b80939fcecc001705c9f4a812fa361764
Maxtasi <magomatioficial@gmail.com>
2023-06-12 23:33:19 -0300

    1.1

--- js/menu.js
+++ js/menu.js
@@ -14,10 +14,7 @@ checkBtn.addEventListener('click', function() {
 });
 
 
-// controla el funcionamiento del boton de wpp
-document.getElementById("whatsapp-btn").onclick = function() {
-  window.location.href = "https://wa.me/3512343686?text=Hola,%20quiero%20info%20sobre%20las%20clases%20de%20taekwondo";
-};
+
 
 
 
@@ -27,8 +24,8 @@ const body = document.getElementById("body");
 check.addEventListener("change", function() {
   if (this.checked) {
     body.style.overflow = "hidden";
+   
   } else {
     body.style.overflow = "auto";
   }
 });
-
